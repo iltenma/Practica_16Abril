bf6a8df83e2aef134cf0b21e4ad1f79b257cc79a
iltenma <36898842+iltenma@users.noreply.github.com>
2018-06-07 10:58:38 +0200

    Add files via upload

---
+++ practica_16abril.ino
@@ -0,0 +1,27 @@
+#include <Servo.h>
+Servo Tatiservo;
+int pot = 0;
+int potV = 0;
+
+void setup() {
+Tatiservo.attach (10); // SIEMPRE PWM
+pinMode (11, OUTPUT);
+Serial.begin (9600);
+
+}
+
+void loop() {
+pot = analogRead (0);
+potV = map (pot, 1, 1023, 1, 170);
+Serial.println (potV);
+
+
+
+//if (potLeer >= 
+
+  Tatiservo.write (potV);
+  delay (1000);
+  Tatiservo.write (potV);
+  delay (1000);
+
+}
